b6c523d522172a02814938f4d59e796ade4d7400
Maia Sosa Kapur <kapurm@uw.edu>
2020-05-03 19:15:15 -0700

    updating individual export

--- R/palettes.R
+++ R/palettes.R
@@ -1,19 +1,22 @@
 #' A muted, qualitative color palette
 #' @import tibble
 #' @import PNWColors
-#' @export
 #' @examples
 #' library(scales)
 #' scales::show_col(sunset_pal()(9))
 
 
+#' @rdname ipsum_pal
+#' @export
 ipsum_palette <- c("#d18975", "#8fd175", "#3f2d54", "#75b8d1", "#2d543d", "#c9d175", "#d1ab75", "#d175b8", "#758bd1") ## hrbr themes
-starfish_palette <- c("#24492e", "#015b58" ,"#2c6184", "#59629b", "#89689d", "#ba7999" ,"#e69b99") ## pnwpal
-sunset_palette <- c( "#41476b", "#675478", "#9e6374" ,"#c67b6f" ,"#de9b71" ,"#efbc82", "#fbdfa2") ## pnwpal
-
-
 ipsum_pal <- function() { manual_pal(ipsum_palette) }
+#' @rdname starfish_pal
+#' @export
+starfish_palette <- c("#24492e", "#015b58" ,"#2c6184", "#59629b", "#89689d", "#ba7999" ,"#e69b99") ## pnwpal
 starfish_pal <- function() { manual_pal(starfish_palette) }
+#' @rdname sunset_pal
+#' @export
+sunset_palette <- c( "#41476b", "#675478", "#9e6374" ,"#c67b6f" ,"#de9b71" ,"#efbc82", "#fbdfa2") ## pnwpal
 sunset_pal <- function() { manual_pal(sunset_palette) }
 
 #' Discrete color & fill scales based on the sunset palette
